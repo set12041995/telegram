97b9996324066583a2eecc9b3719e3aae3ef818f
set12041995 <set12041995@gmail.com>
2023-02-10 01:16:11 +0200

    Finish

--- script.js
+++ script.js
@@ -1,56 +1,20 @@
-const name = prompt("Имя")
-const lostName = prompt("Фамилия")
-const den = +prompt("Дата рождения");
-const month = +prompt("Месяц рождения");
-const yearUser = +prompt("Год рождения")
-let currentlyYear = "2022"
-let year = currentlyYear - yearUser
-let years = year + " Лет"
+let arr = []
+let userNumber = 0
+let res = 0
 
-switch (month) {
-    case 1:
-        den <= 19 ? znak = 'Козерог &#9809' : znak = 'Водолей &#9810';
-        break;
-    case 2:
-        den <= 18 ? znak = 'Водолей &#9810' : znak = 'Рыбы &#9811';
-        break;
-    case 3:
-        den <= 20 ? znak = 'Рыбы &#9811' : znak = 'Овен &#9800';
-        break;
-    case 4:
-        den <= 19 ? znak = 'Овен &#9800' : znak = 'Телец &#9801';
-        break;
-    case 5:
-        den <= 20 ? znak = 'Телец &#9801' : znak = 'Близнецы &#9802';
-        break;
-    case 6:
-        den <= 21 ? znak = 'Близнецы &#9802' : znak = 'Рак &#9803';
-        break;
-    case 7:
-        den <= 22 ? znak = 'Рак &#9803' : znak = 'Лев &#9804';
-        break;
-    case 8:
-        den <= 22 ? znak = 'Лев &#9804' : znak = 'Дева &#9805';
-        break;
-    case 9:
-        den <= 22 ? znak = 'Дева &#9805' : znak = 'Весы &#9806';
-        break;
-    case 10:
-        den <= 22 ? znak = 'Весы &#9806' : znak = 'Скорпион &#9807';
-        break;
-    case 11:
-        den <= 22 ? znak = 'Скорпион &#9807' : znak = 'Стрелец &#9808';
-        break;
-    case 12:
-        den <= 21 ? znak = 'Стрелец &#9808' : znak = 'Козерог &#9809';
-        break;
-}
+while (true) {
+    userNumber = prompt('Enter a number from 2 to 10')
 
-function isLeapYear(yearUser) {
-    return yearUser % 400 === 0 || (yearUser % 100 !== 0 && yearUser % 4 === 0);
-}
-if (isLeapYear(yearUser)) {
-    document.write(`User Bio: ${name}, ${lostName}, ${years}, Высокосный год ${znak}`);
-} else {
-    document.write(`User Bio: ${name}, ${lostName}, ${years}, Не высокосный год ${znak}`);
+    if (Math.abs(userNumber) > 2 && Math.abs(userNumber) < 10) {
+
+        for (i = 0; i < userNumber; i++) {
+            arr.push(Math.round(Math.random() * 10))
+        }
+        for (let el of arr) {
+            res = res + el
+        }
+        console.log(arr)
+        console.log(res)
+        break;
+    }
 }
