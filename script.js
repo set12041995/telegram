c2ebf348cb81e9d6ce4d7dc7ddeebea2ef995d6b
set12041995 <set12041995@gmail.com>
2023-02-28 23:32:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const ITCompany = {
             },
         },
         {
-            scramMaster: {
+            scrumMaster: {
                 salary: 500
             },
         },
@@ -24,24 +24,24 @@ const ITCompany = {
             },
         }
     ]
-}
+};
 
-const nameWorker = prompt('What is your name?');
-const positionWorker = prompt('Your position?').toLowerCase();
-const salaryWorker = +prompt('Desired Salary?');
+function checkWorker() {
+    const nameWorker = prompt('What is your name?');
+    const positionWorker = prompt('Your position?').toLowerCase();
+    const salaryWorker = +prompt('Desired Salary?');
 
-ITCompany.greeting = function () {
-    document.write(`Hello, my name is ${this.name}, I'm ${this.position} in ${this.companyName}`);
-}
+    const worker = {};
 
-function newWorker() {
+    worker.greeting = () => {
+        document.write(`Hello, my name is ${worker.name}, I'm ${worker.position} in ${worker.companyName}`);
+    };
+
     for (let i = 0; i < ITCompany.vacancies.length; i++) {
-        const item = ITCompany.vacancies[i];
-        const position = Object.keys(item)[0].toLowerCase();
+        let position = Object.keys(ITCompany.vacancies[i])[0].toLowerCase();
         if (positionWorker === position) {
-            const salary = item[position].salary;
+            const salary = Object.values(ITCompany.vacancies[i])[0].salary;
             if (salaryWorker === salary) {
-                const worker = Object.create(ITCompany);
                 worker.name = nameWorker;
                 worker.salary = salary;
                 worker.position = position;
@@ -49,12 +49,15 @@ function newWorker() {
                 worker.greeting();
                 return;
             } else {
-                document.write(`${nameWorker}, you has significant skills at ${position} but we hired another developer, let's keep contact !`);
+                document.write(`${nameWorker}, you have significant skills at ${position} but we hired another developer, let's keep in touch!`);
                 return;
             }
+        } else if (positionWorker !== position) {
+            continue;
         }
     }
+
     document.write(`Sorry, we don't have any open positions for ${positionWorker} at the moment.`);
 }
 
-newWorker();
+checkWorker();
