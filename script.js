485ddc595e7de0c8ec95db79952042b25ab135c9
set12041995 <set12041995@gmail.com>
2023-03-07 01:32:11 +0200

    homework done
    homework done

--- script.js
+++ script.js
@@ -1,56 +1,52 @@
-const name = prompt("Имя")
-const lostName = prompt("Фамилия")
-const den = +prompt("Дата рождения");
-const month = +prompt("Месяц рождения");
-const yearUser = +prompt("Год рождения")
-let currentlyYear = "2022"
-let year = currentlyYear - yearUser
-let years = year + " Лет"
+class Hamburger {
+  static SIZE_SMALL = { price: 50, calories: 20 };
+  static SIZE_BIG = { price: 100, calories: 40 };
+  static STUFFING_CHEESE = { price: 10, calories: 20 };
+  static STUFFING_SALAD = { price: 20, calories: 5 };
+  static STUFFING_POTATO = { price: 15, calories: 10 };
+  static TOPPING_MAYO = { price: 20, calories: 5 };
+  static TOPPING_SPICE = { price: 15, calories: 0 };
 
-switch (month) {
-    case 1:
-        den <= 19 ? znak = 'Козерог &#9809' : znak = 'Водолей &#9810';
-        break;
-    case 2:
-        den <= 18 ? znak = 'Водолей &#9810' : znak = 'Рыбы &#9811';
-        break;
-    case 3:
-        den <= 20 ? znak = 'Рыбы &#9811' : znak = 'Овен &#9800';
-        break;
-    case 4:
-        den <= 19 ? znak = 'Овен &#9800' : znak = 'Телец &#9801';
-        break;
-    case 5:
-        den <= 20 ? znak = 'Телец &#9801' : znak = 'Близнецы &#9802';
-        break;
-    case 6:
-        den <= 21 ? znak = 'Близнецы &#9802' : znak = 'Рак &#9803';
-        break;
-    case 7:
-        den <= 22 ? znak = 'Рак &#9803' : znak = 'Лев &#9804';
-        break;
-    case 8:
-        den <= 22 ? znak = 'Лев &#9804' : znak = 'Дева &#9805';
-        break;
-    case 9:
-        den <= 22 ? znak = 'Дева &#9805' : znak = 'Весы &#9806';
-        break;
-    case 10:
-        den <= 22 ? znak = 'Весы &#9806' : znak = 'Скорпион &#9807';
-        break;
-    case 11:
-        den <= 22 ? znak = 'Скорпион &#9807' : znak = 'Стрелец &#9808';
-        break;
-    case 12:
-        den <= 21 ? znak = 'Стрелец &#9808' : znak = 'Козерог &#9809';
-        break;
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+    this.toppings = [];
+  }
+
+  addTopping(topping) {
+    this.toppings.push(topping);
+  }
+
+  removeTopping(topping) {
+    const index = this.toppings.indexOf(topping);
+    if (index !== -1) {
+      this.toppings.splice(index, 1);
+    }
+  }
+
+  get price() {
+    const toppingsPrice = this.toppings.reduce(
+      (acc, topping) => acc + topping.price,
+      0
+    );
+    return this.size.price + this.stuffing.price + toppingsPrice;
+  }
+
+  get calories() {
+    const toppingsCalories = this.toppings.reduce(
+      (acc, topping) => acc + topping.calories,
+      0
+    );
+    return this.size.calories + this.stuffing.calories + toppingsCalories;
+  }
 }
 
-function isLeapYear(yearUser) {
-    return yearUser % 400 === 0 || (yearUser % 100 !== 0 && yearUser % 4 === 0);
-}
-if (isLeapYear(yearUser)) {
-    document.write(`User Bio: ${name}, ${lostName}, ${years}, Высокосный год ${znak}`);
-} else {
-    document.write(`User Bio: ${name}, ${lostName}, ${years}, Не высокосный год ${znak}`);
-}
+const hamburger = new Hamburger(
+  Hamburger.SIZE_SMALL,
+  Hamburger.STUFFING_CHEESE
+);
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
+console.log(`Calories: ${hamburger.calories}`);
+console.log(`Price: ${hamburger.price}`);
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
+console.log(`Price with sauce: ${hamburger.price}`);
