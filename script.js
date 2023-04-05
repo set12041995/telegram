8fb3997fcd108f2b150bff0808fdae9d703dfbd8
set12041995 <set12041995@gmail.com>
2023-04-05 23:25:12 +0300

    Home_Work_17
    Home_Work_17

--- script.js
+++ script.js
@@ -1,56 +1,107 @@
-const name = prompt("Имя")
-const lostName = prompt("Фамилия")
-const den = +prompt("Дата рождения");
-const month = +prompt("Месяц рождения");
-const yearUser = +prompt("Год рождения")
-let currentlyYear = "2022"
-let year = currentlyYear - yearUser
-let years = year + " Лет"
+const userData = {
+  USD: 1000,
+  EUR: 900,
+  UAH: 15000,
+  BIF: 20000,
+  AOA: 100,
+};
 
-switch (month) {
-    case 1:
-        den <= 19 ? znak = 'Козерог &#9809' : znak = 'Водолей &#9810';
-        break;
-    case 2:
-        den <= 18 ? znak = 'Водолей &#9810' : znak = 'Рыбы &#9811';
-        break;
-    case 3:
-        den <= 20 ? znak = 'Рыбы &#9811' : znak = 'Овен &#9800';
-        break;
-    case 4:
-        den <= 19 ? znak = 'Овен &#9800' : znak = 'Телец &#9801';
-        break;
-    case 5:
-        den <= 20 ? znak = 'Телец &#9801' : znak = 'Близнецы &#9802';
-        break;
-    case 6:
-        den <= 21 ? znak = 'Близнецы &#9802' : znak = 'Рак &#9803';
-        break;
-    case 7:
-        den <= 22 ? znak = 'Рак &#9803' : znak = 'Лев &#9804';
-        break;
-    case 8:
-        den <= 22 ? znak = 'Лев &#9804' : znak = 'Дева &#9805';
-        break;
-    case 9:
-        den <= 22 ? znak = 'Дева &#9805' : znak = 'Весы &#9806';
-        break;
-    case 10:
-        den <= 22 ? znak = 'Весы &#9806' : znak = 'Скорпион &#9807';
-        break;
-    case 11:
-        den <= 22 ? znak = 'Скорпион &#9807' : znak = 'Стрелец &#9808';
-        break;
-    case 12:
-        den <= 21 ? znak = 'Стрелец &#9808' : znak = 'Козерог &#9809';
-        break;
+const bankData = {
+  USD: {
+    max: 3000,
+    min: 100,
+    img: "💵",
+  },
+  EUR: {
+    max: 1000,
+    min: 50,
+    img: "💶",
+  },
+  UAH: {
+    max: 0,
+    min: 0,
+    img: "💴",
+  },
+  GBP: {
+    max: 10000,
+    min: 100,
+    img: "💷",
+  },
+};
+
+function getMoney() {
+  return new Promise((resolve, reject) => {
+    let answer;
+    while (answer !== "так" && answer !== "ні") {
+      answer = prompt(
+        `Чи бажаєте подивитись баланс на карті? (введіть 'так' або 'ні')`
+      )
+        .trim()
+        .toLowerCase();
+    }
+    if (answer == "так") {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
 
-function isLeapYear(yearUser) {
-    return yearUser % 400 === 0 || (yearUser % 100 !== 0 && yearUser % 4 === 0);
+getMoney()
+  .then(checkBalance)
+  .catch(takeCash)
+  .finally(() => {
+    console.log("Дякую, гарного дня 😊");
+  });
+
+function checkBalance() {
+  let checkBalanceAnswer = "";
+  while (!userData.hasOwnProperty(checkBalanceAnswer)) {
+    checkBalanceAnswer = prompt(`Баланс якої валюти ви бажаєте подивитись?`)
+      .trim()
+      .toUpperCase();
+  }
+
+  console.log(
+    `Баланс становить ${userData[checkBalanceAnswer]} ${checkBalanceAnswer}.`
+  );
 }
-if (isLeapYear(yearUser)) {
-    document.write(`User Bio: ${name}, ${lostName}, ${years}, Высокосный год ${znak}`);
-} else {
-    document.write(`User Bio: ${name}, ${lostName}, ${years}, Не высокосный год ${znak}`);
+
+function takeCash() {
+  let currencyAnswer = "";
+  const availableCurrencies = checkAvailableCurrency();
+  while (!availableCurrencies.includes(currencyAnswer)) {
+    currencyAnswer = prompt(`Яку валюту бажаєте зняти?`).trim().toUpperCase();
+  }
+
+  let sumToTake;
+  while (isNaN(sumToTake) || sumToTake < 0) {
+    sumToTake = +prompt(`Яку суму бажаєте зняти?`);
+  }
+
+  if (sumToTake > userData[currencyAnswer]) {
+    console.log(`На вашому балансі недостатньо грошей`);
+  } else if (sumToTake > bankData[currencyAnswer].max) {
+    console.log(
+      `Введена сума більша за доступну. Максимальна сума зняття: ${bankData[currencyAnswer].max} `
+    );
+  } else if (sumToTake < bankData[currencyAnswer].min) {
+    console.log(
+      `Введена сума менша за доступну. Мінімальна сума зняття: ${bankData[currencyAnswer].min} `
+    );
+  } else {
+    console.log(
+      `Ось Ваші гроші - ${sumToTake} ${currencyAnswer} ${bankData[currencyAnswer].img}`
+    );
+  }
+}
+
+function checkAvailableCurrency() {
+  const availableInBankomat = Object.keys(bankData).filter(
+    (currency) => bankData[currency].max > 0
+  );
+  const availableInUserData = Object.keys(userData).filter((currency) =>
+    availableInBankomat.includes(currency)
+  );
+  return availableInUserData;
 }
